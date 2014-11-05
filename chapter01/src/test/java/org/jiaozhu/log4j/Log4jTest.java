53c2365e39c0fdf689c402cf466dd52d8fc89719
jiaozhu <gitview@gmail.com>
2014-11-05 16:33:28 +0800

    增加log4配置

--- chapter01/src/test/java/org/jiaozhu/log4j/Log4jTest.java
+++ chapter01/src/test/java/org/jiaozhu/log4j/Log4jTest.java
@@ -12,7 +12,7 @@ import org.apache.log4j.Logger;
 public class Log4jTest {
 
     public void testConsleAppender(){
-        Logger logger = Logger.getLogger("console");
+        Logger logger = Logger.getLogger(Log4jTest.class);
         logger.error("级别信息");
         logger.info("级别信息");
         logger.debug("级别信息");
